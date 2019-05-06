ef857ff0a26cfb278415913dc42273cd2f61e5e9
James <thatoneddrguy@gmail.com>
2019-05-05 21:54:37 -0700

    Figuring out form stuff

--- teachers.php
+++ teachers.php
@@ -17,32 +17,49 @@ mysql_select_db($username, $link);
 <body>
 <a href="index.html">aww shit im not supposed to be here, take me back</a>
 <h1>YOU'VE COME TO THA TEACHER PAGE. YOU BEST BE A TEACHER IF YOU HERE, FO' REAL!</h1>
-<table>
-	<tr>
-		<th>SSN</th>
-		<th>Name</th>
-		<th>Telephone</th>
-		<th>Sex</th>
-		<th>Title</th>
-		<th>Salary</th>
-		<th>Address</th>
-	</tr>
-	<?php
-	$result = mysql_query("SELECT * FROM PROFESSOR;", $link);
-	for($i = 0; $i < mysql_numrows($result); $i++)
+<form action="teachers.php" method="POST">
+Enter a professor's social security number:<input type="text" name="ssn" />
+<input type="submit" />
+</form>
+<br>
+<?php
+if ($_POST["ssn"] != "")
+{
+	$result = mysql_query("SELECT * FROM PROFESSOR WHERE SSN='".$_POST["ssn"]."';", $link);
+	
+	if(mysql_numrows($result) == 0)
 	{
-		echo "<tr>";
-		echo "<td>".mysql_result($result, $i, "SSN")."</td>";
-		echo "<td>".mysql_result($result, $i, "NAME")."</td>";
-		echo "<td>".mysql_result($result, $i, "TELEPHONE")."</td>";
-		echo "<td>".mysql_result($result, $i, "SEX")."</td>";
-		echo "<td>".mysql_result($result, $i, "TITLE")."</td>";
-		echo "<td>".mysql_result($result, $i, "SALARY")."</td>";
-		echo "<td>".mysql_result($result, $i, "ADDRESS")."</td>";
-		echo "</tr>";
+		echo "SSN '".$_POST["ssn"]."' not found in database.";
 	}
-	mysql_close($link);
-	?>
-</table>
+	else
+	{
+		echo "<table>";
+		echo "<tr>";
+		echo "<th>SSN</th>";
+		echo "<th>Name</th>";
+		echo "<th>Telephone</th>";
+		echo "<th>Sex</th>";
+		echo "<th>Title</th>";
+		echo "<th>Salary</th>";
+		echo "<th>Address</th>";
+		echo "</tr>";
+
+		for($i = 0; $i < mysql_numrows($result); $i++)
+		{
+			echo "<tr>";
+			echo "<td>".mysql_result($result, $i, "SSN")."</td>";
+			echo "<td>".mysql_result($result, $i, "NAME")."</td>";
+			echo "<td>".mysql_result($result, $i, "TELEPHONE")."</td>";
+			echo "<td>".mysql_result($result, $i, "SEX")."</td>";
+			echo "<td>".mysql_result($result, $i, "TITLE")."</td>";
+			echo "<td>".mysql_result($result, $i, "SALARY")."</td>";
+			echo "<td>".mysql_result($result, $i, "ADDRESS")."</td>";
+			echo "</tr>";
+		}
+		mysql_close($link);
+		echo "</table>";
+	}
+}
+?>
 </body>
 </html>
